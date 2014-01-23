dbc61071c7369d67644766d5fb0c684e77dafc26
coolsea <wei_918@coolsea.net>
2014-01-23 23:53:10 +0800

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,60 +2,50 @@ require 'spec_helper'
 
 describe "StaticPages" do
  
-	describe "首頁 static_pages/home" do
-
-	  it "首頁頁面內容要含有'Sample App'" do
-	    visit '/static_pages/home'
-	    expect(page).to have_content('Sample App')
-	  end
-	end
 
 	describe "首頁 static_pages/home" do
+		  before {visit root_path} 
 
-	  it "首頁頁面標題要含有'Ruby on Rails | Home'" do
-	    visit '/static_pages/home'
-	    expect(page).to have_title('Ruby on Rails | Home')
-	  end
+		  it "首頁頁面內容要含有'Sample App'" do
+		    expect(page).to have_content('Sample App')
+		  end
+
+		  it "首頁頁面標題要含有'Ruby on Rails | Home'" do
+		    expect(page).to have_title('Ruby on Rails')
+		  end
+
 	end
 
 	describe "幫助頁面 static_pages/help" do
+		before {visit '/help'} 
 
-	  it "幫助頁面首頁要含有'Help Me'" do
-	    visit '/static_pages/help'
-	    expect(page).to have_content('Help Me')
-	  end
-	end
+		  it "幫助頁面首頁要含有'Help Me'" do
+		    expect(page).to have_content('Help Me')
+		  end
 
-	describe "幫助頁面 static_pages/help" do
-
-	  it "幫助頁面標題要含有'Ruby on Rails | Help'" do
-	    visit '/static_pages/help'
-	    expect(page).to have_title('Ruby on Rails | Help')
-	  end
+		  it "幫助頁面標題要含有'Ruby on Rails | Help'" do
+		    expect(page).to have_title('Ruby on Rails | Help')
+		  end
 	end
 
 	describe "關於我頁面 static_pages/about" do
+		before {visit '/about'} 
 
-	  it "關於我頁面要含有'About Me'" do
-	    visit '/static_pages/about'
-	    expect(page).to have_content('About Me')
-	  end
-	end
+		it "關於我頁面要含有'About Me'" do
+		  expect(page).to have_content('About Me')
+		end
 
-	describe "關於我 static_pages/about" do
-
-	  it "首頁頁面標題要含有'Ruby on Rails | About'" do
-	    visit '/static_pages/about'
-	    expect(page).to have_title('Ruby on Rails | About')
-	  end
+		it "首頁頁面標題要含有'Ruby on Rails | About'" do
+		  expect(page).to have_title('Ruby on Rails | About')
+		end
 	end
 
 	describe "聯絡我 static_pages/contact" do
+		before {visit '/contact'} 
 
-	  it "首頁頁面標題要含有'Ruby on Rails | Contact'" do
-	    visit '/static_pages/contact'
-	    expect(page).to have_title('Ruby on Rails | Contact')
-	  end
+		it "首頁頁面標題要含有'Ruby on Rails | Contact'" do
+		  expect(page).to have_title('Ruby on Rails | Contact')
+		end
 	end
 
 
