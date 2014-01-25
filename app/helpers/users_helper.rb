ae8b91052d1fe083cb8fcb1f1339903c8a309a2d
coolsea <wei_918@coolsea.net>
2014-01-25 14:30:12 +0800

    add users-routes & show-index & gravatar圖像

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+	#使用gravatar服務，來顯示頭像，email地址會經過加密，然後再來獲取頭像
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
