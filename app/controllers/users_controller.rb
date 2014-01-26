68e39c781616a73b3e168e4e60575e547e0a7bd6
coolsea <wei_918@coolsea.net>
2014-01-26 17:13:45 +0800

    只有owner才能編輯自己個人資料

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :corrent_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -38,6 +41,16 @@ class UsersController < ApplicationController
 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def signed_in_user
+    store_location
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def corrent_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
