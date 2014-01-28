c0fda5f80a560f1f23ed66228a7de85562ece59b
coolsea <wei_918@coolsea.net>
2014-01-28 16:01:20 +0800

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,11 @@ class Micropost < ActiveRecord::Base
 	validates :user_id, presence: true
 	validates :content, presence: true, length: { maximum: 140 }
 
+# Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+          followed_user_ids: followed_user_ids, user_id: user)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
 
     has_many :microposts, dependent: :destroy
+    has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+
+    has_many :reverse_relationships, foreign_key: "followed_id",
+                                        class_name: "Relationship",
+                                        dependent: :destroy
+    has_many :followers, through: :reverse_relationships
+    has_many :followed_users, through: :relationships, source: :followed
+
+
 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
@@ -26,8 +35,24 @@ class User < ActiveRecord::Base
 
     #feed
     def feed
-        Micropost.where("user_id = ?",id)
+        Micropost.from_users_followed_by(self)
     end
+
+    #檢查一個用戶是否關注了另一個用戶
+    def following?(other_user)
+        relationships.find_by(followed_id: other_user.id)
+    end
+
+    #可以創造關注
+    def follow!(other_user)
+        relationships.create!(followed_id: other_user.id)
+    end
+
+    #可以取消關注
+    def unfollow!(other_user)
+        relationships.find_by(followed_id: other_user.id).destroy
+    end
+
 
     private
 
